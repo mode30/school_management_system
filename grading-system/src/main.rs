89d5133cd4a9c0d3911e73b523e63885e62708c1
mode <emmanuelmodebelu@gmail.com>
2026-05-21 03:25:40 +0100

    added  student type

--- grading-system/src/main.rs
+++ grading-system/src/main.rs
@@ -1,42 +1,61 @@
+use core::fmt;
 // use core::fmt;
-use std::{collections::HashMap, io};
-//Gradebook mapping to name and grade
-#[derive(Debug)]
+use std::collections::HashMap;
 #[allow(dead_code)]
-struct GradeBook {
-    person: HashMap<String, Vec<Option<u8>>>,
+
+struct Students {
+    name:String,
+    //string -> courses:options<u8>-> grades
+    grades:HashMap<String,Option<u8>>
+    // students: Vec<GradeBook>,
 }
 
 fn main() {
-    let person_1_name = String::from("benjamin");
-    let mut person_1_container = Vec::new();
-    let person_1_grade = Some(8);
-    person_1_container.push(person_1_grade);
-    let mut person: HashMap<String, Vec<Option<u8>>> = HashMap::new();
-    person.insert(person_1_name, person_1_container);
-    let grade_1 = GradeBook::new(person);
+    let mut person_1 = Students::new("benjamin".to_owned());
 
-    println!("person:{:?}", grade_1);
-    // println!("person:{}",grade_1);
+    person_1.add_grade("benjamin".to_owned(), Some(9));
+    person_1.add_grade("carson".to_owned(), Some(10));
+    person_1.add_grade("kevin".to_owned(), Some(3));
+    person_1.add_grade("jonathan".to_owned(), Some(11));
+    person_1.add_grade("kanyinsola".to_string(), Some(88));
 
-    grade_1.add_grade("benjamin", Some(22));
 }
 
 #[allow(dead_code)]
-impl GradeBook {
-    fn new(person: HashMap<String, Vec<Option<u8>>>) -> Self {
-        GradeBook { person }
-    }
-
-    #[allow(dead_code)]
-    fn add_grade(&self, _name: &str, _grade: Option<u8>) {
-        for names in &self.person.keys() {
-            println!("names")
-
-            // println!("{:?}",names)
+impl Students {
+    fn new(name:String) -> Self {
+        Students {
+            name,
+            grades: HashMap::new(),
         }
     }
+
+    // #[allow(dead_code)]
+    fn add_grade(&mut self, _course: String, _grade: Option<u8>) {
+        self.grades.insert(_course, _grade);
+    }
+    fn display_information(&self){
+
+    }
 }
+
+impl fmt::Display for Students{
+    fn fmt (&self,f:&mut fmt::Formatter<'_>) ->fmt::Result{
+        write!(f,"information:names{}\ngrades:{}",self.name,self.grades)
+    }
+}
+
+// impl Students{
+//     fn new() -> Self {
+//         Students {
+//             students: Vec::new(),
+//         }
+//     }
+
+//     fn add_students(&mut self, students: Vec<Students>) {
+//         self.student
+//     }
+// }
 
 // impl fmt::Display for GradeBook{
 //     fn fmt(&self,f:&mut fmt::Formatter<'_>)->fmt::Result{
@@ -44,16 +63,16 @@ impl GradeBook {
 //     }
 // }
 
-fn new_person(name: String, grade: u8) -> Result<HashMap<String, Vec<Option<u8>>>, io::Error> {
-    if name.is_empty() {
-        return Err(io::Error::new(io::ErrorKind::NotFound, "cannot be empty"));
-    }
-    let person_1_name = String::from(name);
-    let mut person_1_container = Vec::new();
-    let person_1_grade = Some(grade);
-    person_1_container.push(person_1_grade);
-    let mut person: HashMap<String, Vec<Option<u8>>> = HashMap::new();
-    person.insert(person_1_name, person_1_container);
-    Ok(person)
-    // let grade_1=GradeBook::new(person);
-}
+// fn new_person(name: String, grade: u8) -> Result<HashMap<String, Vec<Option<u8>>>, io::Error> {
+//     if name.is_empty() {
+//         return Err(io::Error::new(io::ErrorKind::NotFound, "cannot be empty"));
+//     }
+//     let person_1_name = String::from(name);
+//     let mut person_1_container = Vec::new();
+//     let person_1_grade = Some(grade);
+//     person_1_container.push(person_1_grade);
+//     let mut person: HashMap<String, Vec<Option<u8>>> = HashMap::new();
+//     person.insert(person_1_name, person_1_container);
+//     Ok(person)
+//     // let grade_1=GradeBook::new(person);
+// }
